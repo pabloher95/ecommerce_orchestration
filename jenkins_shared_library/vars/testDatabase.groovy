fe51c0d030a89d82258c333ee771b40c5de884d7
pabloher95 <phernandezpedraz@uchicago.edu>
2026-03-12 03:00:12 -0500

    refactor folders

--- jenkins_shared_library/vars/testDatabase.groovy
+++
@@ -1,6 +0,0 @@
-def call() {
-  node {
-    echo 'Database schema validation - no automated tests'
-    // Optional: add psql dry-run or sqlfluff if available
-  }
-}
--- jenkins_shared_library/vars/testNode.groovy
+++
@@ -1,7 +0,0 @@
-def call() {
-  node {
-    if (fileExists('package.json') && sh(script: 'grep -q "\"test\"" package.json', returnStatus: true) == 0) {
-      sh 'npm test -- --passWithNoTests --watchAll=false'
-    }
-  }
-}
